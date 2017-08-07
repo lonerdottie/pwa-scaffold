53691e007cf13c5c9afccfcd6ab7d1dd73ee6d4b
David Orr <enki.eridu@gmail.com>
2017-08-07 17:31:54 +0000

    added sample firebaseConfig.js

---
+++ src/helpers/firebaseConfig-sample.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: '<your-api-key>',
+  authDomain: '<your-project-id>.firebaseapp.com',
+  databaseURL: 'https://<your-project-id>.firebaseio.com',
+  projectId: '<your-project-id>',
+  storageBucket: '<your-project-id>.appspot.com',
+  messagingSenderId: '<your-sender-ID>'
+}
